5696154b07aaf56f15ad44aa3d7cd146bfced88f
yugo <yugosakamoto0717@gmail.com>
2023-10-08 14:25:35 -0300

    [feat] Implements login functionality

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import ResetCSS from "./styles/ResetCss";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
 import "./App.css";
+import { useState } from "react";
+import UserContext from "./contexts/UserContext";
 
 function App() {
+  const [userInfo, setUserInfo] = useState({});
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-      <ResetCSS />
-    </BrowserRouter>
+    <UserContext.Provider value={{ userInfo, setUserInfo }}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+        <ResetCSS />
+      </BrowserRouter>
+    </UserContext.Provider>
   );
 }
 
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,14 +1,55 @@
-import { Link } from "react-router-dom";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
 import { Container, Form } from "./StyledLogin.js";
+import { useState, useContext } from "react";
+import UserContext from "../../contexts/UserContext.js";
 
 export default function Login() {
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { setUserInfo } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  async function submitLogin(e) {
+    e.preventDefault();
+    const regEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (regEmail.test(userData.email) && userData.password !== "") {
+      const promise = axios.post("http://localhost:4000/login", userData);
+      promise
+        .then((res) => {
+          setUserInfo({ token: res.data.token, name: res.data.name });
+          navigate("/home");
+        })
+        .catch((err) => alert(err));
+    }
+  }
+
   return (
     <Container>
       <h1>MyWallet</h1>
-      <Form>
-        <input type="text" id="email" placeholder="E-mail" required />
+      <Form onSubmit={submitLogin}>
+        <input
+          type="text"
+          id="email"
+          placeholder="E-mail"
+          value={userData.email}
+          onChange={(e) => setUserData({ ...userData, email: e.target.value })}
+          required
+        />
 
-        <input type="password" id="password" placeholder="Senha" required />
+        <input
+          type="password"
+          id="password"
+          placeholder="Senha"
+          value={userData.password}
+          onChange={(e) =>
+            setUserData({ ...userData, password: e.target.value })
+          }
+          required
+        />
 
         <button>Entrar</button>
       </Form>
